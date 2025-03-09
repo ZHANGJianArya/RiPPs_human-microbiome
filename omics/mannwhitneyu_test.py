a9adaf9543db433d97db6f885a6d1ff5945339d0
ZHANGJian <162699326+ZHANGJianArya@users.noreply.github.com>
2025-03-09 18:17:28 +0800

    Update mannwhitneyu_test.py

--- omics/mannwhitneyu_test.py
+++ omics/mannwhitneyu_test.py
@@ -1,56 +1,61 @@
-
 import pandas as pd
 import scipy.stats as stats
 
-'''
 def check_normality(data):
-    test_stat_normality, p_value_normality=stats.shapiro(data)
-    return check_normality
-'''
+    """
+    Check the normality of the data using the Shapiro-Wilk test.
 
-df = pd.read_csv("RiPP_TPM_IBD_MT_cluster90_p12_stat.tsv",encoding="UTF-8",engine = "python",sep = "\t",index_col=0)
+    Parameters:
+    data (array-like): The data to test for normality.
 
-# get a list of all columns in the dataframe without the Group column
+    Returns:
+    float: The test statistic for the Shapiro-Wilk test.
+    float: The p-value for the Shapiro-Wilk test.
+    """
+    test_stat_normality, p_value_normality = stats.shapiro(data)
+    return test_stat_normality, p_value_normality
+
+# Read the input data file
+df = pd.read_csv("RiPP_TPM_IBD_MT_cluster90_p12_stat.tsv", encoding="UTF-8", engine="python", sep="\t", index_col=0)
+
+# Get a list of all columns in the dataframe without the Group column
 column_list = [x for x in df.columns if x != 'Group']
-# create an empty dictionary
+
+# Create empty dictionaries to store test results
 tuc_test_results = {}
 tcd_test_results = {}
-# loop over column_list and execute code explained above
 
+# Loop over column_list and perform statistical tests
 for column in column_list:
-    group_ctr = df.where(df.Group=="nonIBD").dropna()[column]
-    group_uc = df.where(df.Group=="UC").dropna()[column]
+    group_ctr = df.where(df.Group == "nonIBD").dropna()[column]
+    group_uc = df.where(df.Group == "UC").dropna()[column]
 
     test_stat_normality_ctr, p_value_normality_ctr = stats.shapiro(group_ctr)
     test_stat_normality_uc, p_value_normality_uc = stats.shapiro(group_uc)
-    #print(test_stat_normality_ctr, p_value_normality_ctr)
-    #print(test_stat_normality_uc,p_value_normality_uc)
 
     if p_value_normality_ctr > 0.05 and p_value_normality_uc > 0.05:
-        tuc_test_results[column] = stats.ttest_ind(group_ctr,group_uc)
+        tuc_test_results[column] = stats.ttest_ind(group_ctr, group_uc)
     else:
-    	tuc_test_results[column] = stats.mannwhitneyu(x=group_ctr,y=group_uc,alternative = 'two-sided')
+        tuc_test_results[column] = stats.mannwhitneyu(x=group_ctr, y=group_uc, alternative='two-sided')
 
-results_uc_df = pd.DataFrame.from_dict(tuc_test_results,orient='Index')
-results_uc_df.columns = ['statistic','pvalue']
-
-results_uc_df.to_csv("RiPP_in_IBD_uc_MT_sta_tpm.tsv", sep = "\t", index = True)
+# Convert results to DataFrame and save to CSV
+results_uc_df = pd.DataFrame.from_dict(tuc_test_results, orient='Index')
+results_uc_df.columns = ['statistic', 'pvalue']
+results_uc_df.to_csv("RiPP_in_IBD_uc_MT_sta_tpm.tsv", sep="\t", index=True)
 
 for column in column_list:
-    group_ctr = df.where(df.Group=="nonIBD").dropna()[column]
-    group_cd = df.where(df.Group=="CD").dropna()[column]
+    group_ctr = df.where(df.Group == "nonIBD").dropna()[column]
+    group_cd = df.where(df.Group == "CD").dropna()[column]
 
-    test_stat_normality_ctr, p_value_normality_ctr = stats.shapiro(group_ctr)
+    test_stat_normality_ctr, p_value_normality_cd = stats.shapiro(group_ctr)
     test_stat_normality_cd, p_value_normality_cd = stats.shapiro(group_cd)
-    #print(test_stat_normality_ctr, p_value_normality_ctr)
-    #print(test_stat_normality_cd,p_value_normality_cd)
 
     if p_value_normality_ctr > 0.05 and p_value_normality_cd > 0.05:
-        tcd_test_results[column] = stats.ttest_ind(group_ctr,group_cd)
+        tcd_test_results[column] = stats.ttest_ind(group_ctr, group_cd)
     else:
-        tcd_test_results[column] = stats.mannwhitneyu(x=group_ctr,y=group_cd,alternative = 'two-sided')
+        tcd_test_results[column] = stats.mannwhitneyu(x=group_ctr, y=group_cd, alternative='two-sided')
 
-results_cd_df = pd.DataFrame.from_dict(tcd_test_results,orient='Index')
-results_cd_df.columns = ['statistic','pvalue']
-
-results_cd_df.to_csv("RiPP_in_IBD_cd_MT_sta_tpm.tsv", sep = "\t", index = True)
+# Convert results to DataFrame and save to CSV
+results_cd_df = pd.DataFrame.from_dict(tcd_test_results, orient='Index')
+results_cd_df.columns = ['statistic', 'pvalue']
+results_cd_df.to_csv("RiPP_in_IBD_cd_MT_sta_tpm.tsv", sep="\t", index=True)
